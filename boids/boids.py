faf77ea6941c18717dbdb53019b3c226324dbeba
Nicolas Alonso <nicolas.cobo.21@ucl.ac.uk>
2025-12-01 10:36:24 +0000

    FInal commit on my branch

--- boids/boids.py
+++ boids/boids.py
@@ -13,6 +13,9 @@ import random
 def add_array(array1,array2, index1, index2, threshold): 
     array1[index1] = array1[index1] + (array2[index2] - array2[index1]) * threshold/len(array2)
 
+def check_sum_squared_condition_is_met(array1, array2, index1, index2, threshold): 
+    if (array1[index2] - array1[index1])**2 + (array2[index2]- array2[index1])**2 
+
 
 boids_x=[random.uniform(-450,50.0) for x in range(50)]
 boids_y=[random.uniform(300.0,600.0) for x in range(50)]
@@ -28,6 +31,7 @@ def update_boids(boids):
             add_array(xvs,xs,i,j,0.01)
             add_array(yvs,ys,i,j,0.01)
             #Change this
+
             if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
                 xvs[i]=xvs[i]+(xs[i]-xs[j])
                 yvs[i]=yvs[i]+(ys[i]-ys[j])
